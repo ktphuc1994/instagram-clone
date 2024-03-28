ff92fd796bc4a1e280da928397bda75e2427cf02
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-28 10:35:13 +0700

    add the menu and profile image at the right side of the header component

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,16 @@
 import Image from 'next/image';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/24/outline';
+import { HomeIcon } from '@heroicons/react/24/solid';
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl mx-auto'>
+    <div className='flex items-center justify-between max-w-6xl mx-auto px-4'>
       {/** Left */}
 
-      <div className='h-24 w-24 relative hidden lg:inline-grid cursor-pointer'>
+      <div className='shrink-0 h-24 w-24 relative hidden lg:inline-grid cursor-pointer'>
         <Image
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png'
           alt='instagram-logo'
@@ -14,7 +18,7 @@ export default function Header() {
           className='object-contain'
         />
       </div>
-      <div className='h-24 w-10 relative lg:hidden cursor-pointer'>
+      <div className='shrink-0 h-24 w-10 relative lg:hidden cursor-pointer'>
         <Image
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png'
           alt='instagram-logo'
@@ -24,19 +28,27 @@ export default function Header() {
       </div>
 
       {/** Middle */}
-      <div className='relative mt-1'>
-        <div className='absolute top-1/2 left-2 -translate-y-1/2'>
+      <div className='relative mt-1 pl-4'>
+        <div className='absolute top-1/2 left-6 -translate-y-1/2'>
           <MagnifyingGlassIcon className='h-5 text-gray-500' />
         </div>
         <input
           type='text'
-          className='bg-gray-50 py-2 pl-9 pr-3 border border-gray-500 text-sm rounded-md'
+          className='bg-gray-50 py-2 pl-9 pr-3 border border-gray-500 text-sm rounded-md w-full'
           placeholder='Search'
         />
       </div>
 
       {/** Right */}
-      <h1>Right side</h1>
+      <div className='shrink-0 flex space-x-4 items-center'>
+        <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+        <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+        <img
+          src='https://media.licdn.com/dms/image/C5603AQEeDVn0pnKpDQ/profile-displayphoto-shrink_800_800/0/1623814405277?e=2147483647&v=beta&t=6KsthbGW6_lqQg_GVW7rjiaeHk9rs4-uJx1eN_4OYtg'
+          alt='user-image'
+          className='h-10 w-10 rounded-full cursor-pointer'
+        />
+      </div>
     </div>
   );
 }
